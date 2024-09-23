b7335df3551e7209747d5681c0adfe1f8a01db20
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2024-09-22 20:12:46 -0500

    portfolio add-on, is it prime?

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -6,7 +6,8 @@ public class Factorizer {
   /*
    * This method is intended to take a positive int as a parameter and calculates
    * all of the prime number factors of it.
-   * @param list of prime number factors
+   * @param number
+   * @return list of prime num factors 
    */
 
   public List<Integer> primeFactors(int number) {
@@ -32,5 +33,28 @@ public class Factorizer {
     }
 
     return factors;
+  }
+
+  public boolean isPrime(int number) {
+    int initial = number;
+    
+    // Check for number less than or equal to 1
+    if(number <=1) {
+      return false;
+    }
+
+    // Factors out all 2's using modulo division
+    while(number % 2 == 0) {
+      number /= 2;
+    }
+
+    // Factors out all odds using modulo division
+    for(int i = 3; i <= Math.sqrt(number); i+=2) {
+      while(number % i == 0) {
+        number /= i;
+      }
+    }
+    
+    return initial == number;
   }
 }
--- src/test/java/com/csc/TestFactorizer.java
+++ src/test/java/com/csc/TestFactorizer.java
@@ -16,40 +16,60 @@ public class TestFactorizer {
   }
 
   @Test
-  public void testOne() {
+  public void testNumber12() {
     assertEquals(Arrays.asList(2,2,3), factorizer.primeFactors(12));
   }
 
   @Test
-  public void testTwo() {
+  public void testNumber35() {
     assertEquals(Arrays.asList(5,7), factorizer.primeFactors(35));
   }
 
   @Test
-  public void testThree() {
+  public void testNumber48() {
     assertEquals(Arrays.asList(2, 2, 2, 2, 3), factorizer.primeFactors(48));
   }
 
   @Test
-  public void testFour() {
+  public void testNumber13() {
     assertEquals(Arrays.asList(13), factorizer.primeFactors(13));
   }
 
   // Large number case
   @Test
-  public void testFive() {
+  public void testLargeNumber() {
     assertEquals(Arrays.asList(2, 3, 5, 7), factorizer.primeFactors(210));
   }
 
   // Number 1 case
   @Test
-  public void testSix() {
+  public void testSmallNumber() {
     assertEquals(Arrays.asList(), factorizer.primeFactors(1));
   }
 
-  // Negative number case
+  // Portfolio Add-On, isPrime tests
   @Test
-  public void testSeven() {
-    assertEquals(Arrays.asList(), factorizer.primeFactors(-10));
+  public void testIfNumber36IsPrime() {
+    assertEquals(false, factorizer.isPrime(36));
+  }
+
+  @Test
+  public void testIfNumber12IsPrime() {
+    assertEquals(false, factorizer.isPrime(12));
+  }
+
+  @Test
+  public void testIfNumber17IsPrime() {
+    assertEquals(true, factorizer.isPrime(17));
+  }
+
+  @Test
+  public void testIfNumber1IsPrime() {
+    assertEquals(false, factorizer.isPrime(1));
+  }
+
+  @Test
+  public void testIfNumber0IsPrime() {
+    assertEquals(false, factorizer.isPrime(0));
   }
 }
